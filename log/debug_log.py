e26f7e31587c407470da8ffe384df8c848532e1f
miyuuk1 <il.ionow@yandex.ru>
2023-05-29 20:59:12 +0300

    common

---
+++ log/debug_log.py
@@ -0,0 +1,10 @@
+def debug_print(st):
+    print("DEBUG: " + str(st))
+
+
+def debug_array_print(st, arr):
+    print("DEBUG.ARRAY: " + str(st))
+
+    for i in range(len(arr)):
+        print(arr[i])
+
